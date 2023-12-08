cddbfa8e337539b5825df58a70d8db1a37e50b6a
Matthew Chappelle <136776175+MatthewChappelle@users.noreply.github.com>
2023-12-07 22:34:06 -0600

    added write file functionality and tests for shapes

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const { prompt } = require("inquirer");
+const { writeFile } = require("fs");
 const { Circle, Triangle, Square } = require("./lib/shapes");
 
+//will not let answer be blank
+function validateNotEmpty(input) {
+    if (input.length > 0) {
+        return true;
+    }
+    return "Cannot be blank";
+};
+
+//ask user for input for their shape
 const promptUser = async () => {
     const answers = await prompt([
         {
@@ -8,12 +18,19 @@ const promptUser = async () => {
             message:
                 "Logo text? (up to three characters)",
             name: "text",
+            validate: function (input) {
+                if (input.length > 3) {
+                    return "Logo text must no more than three characters.";
+                }
+                return validateNotEmpty(input);
+            }
         },
         {
             type: "input",
             message:
                 "Text color (name OR a hexadecimal number)",
             name: "textColor",
+            validate: validateNotEmpty
         },
         {
             type: "list",
@@ -26,8 +43,60 @@ const promptUser = async () => {
             message:
                 "Shape color (name OR a hexadecimal number)",
             name: "shapeColor",
+            validate: validateNotEmpty
         },
     ]);
+
+    //calls next function
+    writeToFile("logo.svg", answers);
 }
+//creates logo
+const writeToFile = (fileName, answers) => {
+    //asigns empty string
+    let svgString = "";
+    //adds svg tag with basic info
+    svgString = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
+    svgString += "<g>";
+    //adds shape to svg
+    svgString += `${answers.shape}`;
+
+    //determines which shape to use based on input
+    let shapeChoice;
+
+    //filters through answers
+    if (answers.shape === "Circle") {
+        shapeChoice = new Circle();
+        shapeChoice.setColor(answers.shapeColor);
+        svgString += shapeChoice.render();
+    } else if (answers.shape === "Square") {
+        shapeChoice = new Square();
+        shapeChoice.setColor(answers.shapeColor);
+        svgString += shapeChoice.render();
+    } else {
+        shapeChoice = new Triangle();
+        shapeChoice.setColor(answers.shapeColor);
+        svgString += shapeChoice.render();
+    };
+
+    //adds text to svg
+    svgString += `<text x="150" y="110" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+    svgString += "</g>";
+    svgString += "</svg>";
+
+    //writes file and saves it
+    writeFile(fileName, svgString, (err) => {
+        err ? console.log(err) : console.log("Generated logo.svg");
+    });
+};
+
+
+
+
+
+
+
+
+
+
 
 promptUser();
